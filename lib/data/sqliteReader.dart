2f287294416e4c60d8d2750f37ff0dbb119cc75f
klinkl <108584514+klinkl@users.noreply.github.com>
2025-12-04 14:46:04 +0100

    added support to read .apkg files to sqlite database

--- lib/data/sqliteReader.dart
+++ lib/data/sqliteReader.dart
@@ -1,11 +1,54 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:funlearn_client/data/archiveExtractor.dart';
 import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
-Future<void> sqliteReader() async{
-  final path = archiveExtractor();
-  final filename = p.join(path as String, "collection.anki2");
-  print(filename);
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'databaseHelper.dart';
+import 'models/card.dart';
+import 'models/deck.dart';
 
-  var db = await openDatabase('anki.db');
-  
+Future<void> sqliteReader() async{
+  sqfliteFfiInit();
+
+  databaseFactory = databaseFactoryFfi;
+
+  final path = await archiveExtractor();
+  print(path);
+  final filename = p.join(path as String, "collection.anki2");
+  final appDocDir = await getApplicationDocumentsDirectory();
+  final targetDbPath = p.join(appDocDir.path, "collection.anki2");
+  final sourceFile = File(filename);
+  final targetFile = File(targetDbPath);
+  if (await targetFile.exists()){
+    await targetFile.delete();
+  }
+  await sourceFile.copy(targetDbPath);
+  final dbHelper = DatabaseHelper();
+  //read the collection.anki2
+  final ankidb = await openDatabase(targetDbPath, readOnly: true);
+  final db = await dbHelper.database;
+  final deckJSON = await ankidb.query("col",columns: ["decks"]);
+  print(deckJSON.runtimeType);
+  final deck = jsonDecode(deckJSON.first["decks"] as String) as Map<String, dynamic>;
+  deck.forEach((key, value) async {
+    final deckId = key;
+    final name = value["name"];
+    print("ID: $deckId   | Name: $name");
+    //from the database structure as defined here: https://github.com/ankidroid/Anki-Android/wiki/Database-Structure
+    final query = await ankidb.rawQuery("SELECT DISTINCT notes.sfld, notes.flds from notes, cards WHERE cards.nid = notes.id AND ? = cards.did;", [deckId]);
+    print(query.length);
+    if (query.isNotEmpty) {
+      final deckId = await dbHelper.insertDeck(Deck(name: name));
+      for (var row in query) {
+        await dbHelper.insertCard(Card(deckId: deckId,front: row["sfld"] as String, back: row["flds"] as String));
+      }
+      (await dbHelper.getCardsByDeck(deckId))
+          .forEach((row) => print('${row.deckId}${row.cardId}${row.front}${row.back}'));
+
+    }
+  });
+
 }
--- lib/screens/cards_list_view.dart
+++ lib/screens/cards_list_view.dart
@@ -24,15 +24,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final int _counter = 0;
 
-  void addNewDeck() {
-    setState(() async {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-
-        await sqliteReader();
+  void addNewDeck() async{
+    await sqliteReader();
+    setState((){
     });
   }
 
